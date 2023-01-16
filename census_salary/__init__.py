433c45ed2f435043c35142a4a901ae950cc9d635
Mikel <mxagar@gmail.com>
2023-01-16 16:39:23 +0100

    add imports

--- census_salary/__init__.py
+++ census_salary/__init__.py
@@ -1,1 +1,9 @@
-
+from .ml.model import (train_model,
+                       compute_model_metrics,
+                       inference)
+from .ml.data import process_data
+from .train_model import (run_setup,
+                          run_processing,
+                          run_training,
+                          run_evaluation,
+                          run_inference)
--- census_salary/ml/__init__.py
+++ census_salary/ml/__init__.py
@@ -1,1 +1,4 @@
-
+from .data import process_data
+from .model import (train_model,
+                    compute_model_metrics,
+                    inference)

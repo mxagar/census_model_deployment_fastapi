e8903074c083f1a361f25e6b80815d0394684392
Mikel <mxagar@gmail.com>
2023-01-17 08:24:01 +0100

    update interface names

--- census_salary_lib/__init__.py
+++ census_salary_lib/__init__.py
@@ -2,8 +2,8 @@ from .ml.model import (train_model,
                        compute_model_metrics,
                        inference)
 from .ml.data import process_data
-from .train_model import (run_setup,
+from .census_library import (run_setup,
                           run_processing,
-                          run_training,
-                          run_evaluation,
-                          run_inference)
+                          train_pipeline,
+                          load_pipeline,
+                          predict)
